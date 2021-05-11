fca9fdeb36e13320e0d45c12d0d7a41cefe6a7fe
Justin <justin.mcmahon@pliancy.com>
2021-05-11 08:21:38 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint/eslint-plugin', 'unused-imports', 'prettier'],
+    extends: ['prettier'],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'no-console': 'error',
+        'space-before-function-paren': ['error', {
+            named: 'never',
+        }],
+        'unused-imports/no-unused-imports': 'error',
+        'prettier/prettier': [
+            'error',
+            {
+                singleQuote: true,
+                trailingComma: 'all',
+                semi: false,
+                printWidth: 100,
+                tabWidth: 4,
+                overrides: [
+                    {
+                        files: [
+                            '**/*.json',
+                            '**/*.yml',
+                            '*rc',
+                            '*ignore',
+                            '*config'
+                        ],
+                        options: {
+                            tabWidth: 2
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+}
